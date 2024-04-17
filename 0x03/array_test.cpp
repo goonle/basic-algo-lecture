48dcb11627b05a4323f1420621ca1fe05c7ce716
goonle <74893925+goonle@users.noreply.github.com>
2024-04-17 22:47:48 +0900

    00x3

--- 0x03/array_test.cpp
+++ 0x03/array_test.cpp
@@ -2,11 +2,19 @@
 using namespace std;
 
 void insert(int idx, int num, int arr[], int& len){
-
+  for(int i = len; i > idx -1 ; i --){
+    arr[i+1] = arr[i];
+  }
+  arr[idx] = num;
+  len++;
 }
 
 void erase(int idx, int arr[], int& len){
-  
+  for(int i=idx; i<len-1; i ++){
+    arr[idx] = arr[idx+1];
+  }
+  arr[len-1] = nullptr;
+  len--;
 }
 
 void printArr(int arr[], int& len){
